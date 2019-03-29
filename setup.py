e0971f48eaad1ed20f1c972624deec9a77a5261b
westurner <@westurner>
2019-03-29 11:54:54 -0400

    RLS: setup.py, pwd/__init__.py: v0.3.5

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='pwd',
-    version='0.3.4',
+    version='0.3.5',
     description="pwd",
     long_description=readme + '\n\n' + history,
     author="Wes Turner",
